c7960b983fac953397e0c628a857c5da52aa8783
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-06-21 23:03:40 -0400

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Button = ({ styles }) => (
-  <button href="https://metaverseofthings-analyitcs.netlify.app/" type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+  <button  type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}><a href="https://metaverseofthings-analyitcs.netlify.app/">
     Get Started
-  </button>
+  </a></button>
 );
 
 export default Button;
