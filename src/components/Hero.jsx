ae316ccdbfb20bcfe6416375413fce24d8aeae42
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-06-21 21:18:29 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,33 +9,27 @@ const Hero = () => {
        
 
         <div className="flex flex-row justify-between items-center w-full">
+          
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          Metaverse<br className="sm:block hidden" />{" of Things "}
-            <span className="text-gradient">ERP</span>{" "}
+          Metaverse Of Things<br className="sm:block hidden" />{" of Things "}
+            <span className="text-gradient">MetaTracker</span>{" "}
           </h1>
+          
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
+          
         </div>
 
    
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-
-       
-The convergence of artificial intelligence, blockchain, 5G connectivity, virtual reality, cloud compute, 
-and other frontier technologies has sparked a technological revolution that is fundamentally transforming 
-the way society operates. While these technologies have evolved independently over time, they are becoming 
-increasingly intertwined and packaged into a new productive asset: intelligent and autonomous machines. 
-This convergence also leverages the power of cloud computing and storage, enabling seamless scalability, 
-vast computational resources, and ubiquitous access to data, further propelling the transformative potential of 
-these technologies.
-       
+          MetaTracker is a cutting-edge cryptocurrency analytical tracker designed to provide you with comprehensive insights and real-time data for all types of cryptocurrencies. 
+          Dive deep into the world of digital assets and gain a profound understanding of market trends, historical performance, and future projections.
         </p>
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} alt="billing" className="w-[100%] h-[80%] relative z-[5]" />
-
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
