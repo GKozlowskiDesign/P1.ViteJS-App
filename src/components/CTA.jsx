719f5f9eae5e57c1d18d4bd770f3a54a48de1cd3
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-06-21 22:44:41 -0400

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,8 +6,7 @@ const CTA = () => (
     <div className="flex-1 flex flex-col">
       <h2 className={styles.heading2}>Let’s try our service now!</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
+        Dive deep into the world of digital assets and gain a profound understanding of market trends, historical performance, and future projections.
       </p>
     </div>
 
