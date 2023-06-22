4656e698b2abc8f7d6a18f31311bc98b613915ef
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-06-21 21:20:05 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className="flex flex-row justify-between items-center w-full">
           
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          Metaverse Of Things<br className="sm:block hidden" />{" of Things "}
+          Metaverse<br className="sm:block hidden" />{"Of Things"}
             <span className="text-gradient">MetaTracker</span>{" "}
           </h1>
           
