6cec5e0256369a605d00240ec195f54e545f6a86
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-06-26 21:24:49 -0400

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ styles }) => (
   <button  type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}><a href="https://metaversetracker.netlify.app/">
-    Get Started
+    Open dApp
   </a></button>
 );
 
