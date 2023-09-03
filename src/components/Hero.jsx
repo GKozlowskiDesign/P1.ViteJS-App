e33455bfab18aaa7795409f27a657f9c2ad77e12
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-03 19:13:51 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "../style";
-import { discount, robot } from "../assets";
+import { discount, platform } from "../assets";
 
 const Hero = () => {
   return (
@@ -27,6 +27,8 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+    <img src={platform} alt="platform" className="w-[100%] h-[100%] relative z-[5]" />
+
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
