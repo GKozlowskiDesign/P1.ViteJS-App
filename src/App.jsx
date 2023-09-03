d6f181063efda44fae00563ba754e16e3aa2daa6
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-03 19:30:40 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
 import styles from './style'
-import {  CTA, Footer, Clients, CardDeal, Navbar, Stats, Testimonials, Hero, Analytics } from "./components";
-
+import { CTA, Footer, Clients, CardDeal, Navbar, Stats, Testimonials, Hero, Analytics } from "./components";
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
- <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+    
+    {/* Nav Content */} 
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-     <Navbar />
-      </div>
-      </div>
-
-      <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-     
-
+         <Navbar />
       </div>
     </div>
-
+    
+    {/* Intro Content */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    {/* Main Content */}
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Analytics />
         <CardDeal />
         <Testimonials />
-       
         <Clients />
         <CTA />
         <Footer />
       </div>
     </div>
-
+    
   </div>
 );
-
 export default App;
