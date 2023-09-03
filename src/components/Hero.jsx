87c6d7667515a11f576979d7c962e649b324538f
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-03 19:43:29 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,12 +11,10 @@ const Hero = () => {
           <div className="ss:flex hidden md:mr-4 mr-0"></div>  
         </div>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          MetaTracker is a cutting-edge cryptocurrency analytical tracker designed to provide you with comprehensive insights and real-time data for all types of cryptocurrencies. 
-          Dive deep into the world of digital assets and gain a profound understanding of market trends, historical performance, and future projections.
+         The Metaverse Of Things (MVoT) is a groundbreaking web3 blockchain-based Decentralized Autonomous Organization (DAO) 
+         dedicated to advancing the integration of the world into Web3. MVoT's visionary approach encompasses three distinct projects to meet our goal.
         </p>
       </div>
-
-      
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={platform} alt="platform" className="w-1/2 rounded-2xl shadow-2xl relative z-[5]" />
           {/* gradient start */}
