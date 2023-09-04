caf017c25acfe4e90f46a749424e8c5aa6f8e0ff
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-03 20:02:45 -0400

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import gitHub from "./gitHub.png";
 import airbnb from "./airbnb.png";
 import bill from "./bill.png";
 import binance from "./binance.png";
-import card from "./card.png";
+import card from "./PortfolioTools.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
 import logo from "./logo.png";
