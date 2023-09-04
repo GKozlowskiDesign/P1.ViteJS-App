6c32a202f317b8e6049decd8fd857a10db0d4bc6
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-03 20:54:14 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            <span className="text-gradient">Meta</span>{"Tracker"}
+            <span className="text-gradient">Metaverse</span>{"OfThings"}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0"></div>  
         </div>
